1d0c7a960fb415c489d35ae86b78bdf453589fd4
ScottB <scott.bolduc@gmail.com>
2016-10-12 14:16:31 +0000

    updated schema and new add user id to articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012004006) do
+ActiveRecord::Schema.define(version: 20161012140755) do
 
   create_table "articles", force: :cascade do |t|
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|

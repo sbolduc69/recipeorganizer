6e45fd707f20e46b9517b80437086fd16b7707e4
ScottB <scott.bolduc@gmail.com>
2016-12-29 03:42:52 +0000

    added chef models, pages, routes, passwords

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,8 @@ Rails.application.routes.draw do
       post 'like'
     end
   end
+  
+  resources :chefs, except: [:new]
+  
+  get '/register', to: 'chefs#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220235110) do
+ActiveRecord::Schema.define(version: 20161228175447) do
 
 # Could not dump table "articles" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161220235110) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 # Could not dump table "comments" because of following NoMethodError

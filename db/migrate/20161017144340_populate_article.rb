af7657f7ee0cc2736a9d4cff90051d6740f39aa7
ScottB <scott.bolduc@gmail.com>
2017-01-08 22:52:53 +0000

    cleaning up old migrations

--- db/migrate/20161017144340_populate_article.rb
+++
@@ -1,8 +0,0 @@
-class PopulateArticle < ActiveRecord::Migration
-  def change
-    add_column :articles, :title, :string
-    add_column :articles, :text, :text
-    add_column :articles, :datetime, :created_at
-
-  end
-end

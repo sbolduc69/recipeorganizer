30a23bf51734cbd93bed74a6e7413344c55b082d
ScottB <scott.bolduc@gmail.com>
2017-01-08 21:14:42 +0000

    updated carrier_wave.rb configurations

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
     CarrierWave.configure do |config|
-       config.aws_credentials = {
-        :provider => 'AWS',
+        
+      config.storage  = :aws
+      config.aws_bucket = ENV['S3_BUCKET']
+      config.aws_credentials = {
         :access_key_id => ENV['S3_ACCESS_KEY'],
         :secret_access_key => ENV['S3_SECRET_KEY']
        } 
        
-      config.aws_bucket = ENV['S3_BUCKET']
-      
     end
     
 end

9d4134ceba1da179523bcb5a22a9ccd896c38072
ScottB <scott.bolduc@gmail.com>
2017-01-08 21:28:59 +0000

    updated carrierwave region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       config.aws_credentials = {
         :access_key_id => ENV['S3_ACCESS_KEY'],
         :secret_access_key => ENV['S3_SECRET_KEY'],
-        :region =>  ENV.fetch('AWS_REGION')
+        :region =>  ('us-west-2')
        } 
        
     end

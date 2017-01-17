eb15e76d83e4fa64895516f15c5d04f630b5777a
ScottB <scott.bolduc@gmail.com>
2017-01-17 19:15:57 +0000

    add admin functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
   
   def require_user
     if !logged_in?
-      flash[:danger] ="You must be logged in to perform this action"
-      redirect_to (request.referer.present? ? :back : root_path)
+      flash[:danger] = "You must be logged in to perform this action"
+      redirect_to root_path
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109010847) do
+ActiveRecord::Schema.define(version: 20170117184616) do
 
 # Could not dump table "articles" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170109010847) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 # Could not dump table "comments" because of following NoMethodError
